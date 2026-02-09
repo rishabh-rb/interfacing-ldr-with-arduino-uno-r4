1ecfb4669495e785789e98200241cf1b7f8787be
Rishabh-rb <rishabh.barnwal_cs.aiml24@gla.ac.in>
2026-02-09 09:22:25 +0530

    Add LDR sensor reading and configure serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+int ldrPin = A0;
+
 void setup() {
-// write your initialization code here
-    
+    Serial.begin(9600);
+    pinMode(ldrPin, INPUT);
 }
 
 void loop() {
-// write your code here
+    int ldrValue = analogRead(ldrPin);
+    int lightPercent = map(ldrValue, 0, 1023, 0, 100);
+    Serial.println(ldrValue);
+    Serial.println(lightPercent);
+    delay(300);
 }
